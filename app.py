20d08e795ca4fabe227c32b04fcec71822f0a740
MisteryDush <i.arifdjanov@yandex.ru>
2022-08-25 12:53:11 +0800

    Testing database connection and creating entities

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Stall(db.Model):
+    __tablename__ = 'stalls'
+    stall_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=30))
+
+    def __init__(self, name):
+        self.name = name
 
 
 @app.route('/')
@@ -31,6 +50,25 @@ def logout():
 @app.route('/sign-up')
 def sign_up():
     return "Sign Up"
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add():
+    if request.method == 'POST':
+        add_entry(request.form['name'])
+        return redirect(url_for('all'))
+    return render_template('add.html')
+
+
+@app.route('/all')
+def all():
+    return render_template('all.html', stalls=Stall.query.all())
+
+
+def add_entry(name):
+    entry = Stall(name)
+    db.session.add(entry)
+    db.session.commit()
 
 
 if __name__ == '__main__':
