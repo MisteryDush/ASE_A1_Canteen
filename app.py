93cb4068ef9bea957a225b33d67a372bbeea6a0b
MisteryDush <i.arifdjanov@yandex.ru>
2022-08-26 12:22:55 +0800

    Implemented entity retrieval from the db and display it on the page

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 from flask import Flask, render_template, request, redirect, url_for
@@ -17,19 +18,28 @@ class Stall(db.Model):
     __tablename__ = 'stalls'
     stall_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(length=30))
+    filename = db.Column(db.Text)
+    data = db.Column(db.LargeBinary)
 
-    def __init__(self, name):
+    def __init__(self, name, filename, data):
         self.name = name
+        self.filename = filename
+        self.data = data
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+def all_stalls():  # put application's code here
+    stalls = Stall.query.all()
+    for stall in stalls:
+        stall.img = base64.b64encode(stall.data).decode("utf-8")
+    img = stalls[1].img
+    print(len(img))
+    return render_template('index.html', stalls=stalls)
 
 
-@app.route('/Home')
+@app.route('/menu')
 def hello_home():  # put application's code here
-    return render_template('index.html')
+    return render_template('menu.html')
 
 
 @app.route('/settings')
@@ -39,7 +49,7 @@ def settings():
 
 @app.route('/login')
 def login():
-    return "Login"
+    return render_template('login.html')
 
 
 @app.route('/logout')
@@ -55,18 +65,25 @@ def sign_up():
 @app.route('/add', methods=["GET", "POST"])
 def add():
     if request.method == 'POST':
-        add_entry(request.form['name'])
+        file = request.files['file']
+        data = file.read()
+        name = file.filename
+        add_entry(request.form['name'], name, data)
         return redirect(url_for('all'))
     return render_template('add.html')
 
 
 @app.route('/all')
 def all():
-    return render_template('all.html', stalls=Stall.query.all())
+    stalls = Stall.query.all()
+    for stall in stalls:
+        stall.img = base64.b64encode(stall.data).decode("utf-8")
+    print(stalls[0].data)
+    return render_template('all.html', stalls=stalls)
 
 
-def add_entry(name):
-    entry = Stall(name)
+def add_entry(name, filename, data):
+    entry = Stall(name, filename, data)
     db.session.add(entry)
     db.session.commit()
 
